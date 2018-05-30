2ec6e26c85fc6e3e3cd90dd5217a4eb6822fb1f9
Khaled Sharaf Eldin <wi15b189@technikum-wien.at>
2018-05-30 17:12:23 +0200

    WCF now working WebMessageBodystyle needs to be bare not wrapped so we don't need to update the shared datatypes

--- SharedDataTypes/Ingredient.cs
+++ SharedDataTypes/Ingredient.cs
@@ -5,23 +5,12 @@ namespace SharedDataTypes
 {
     public class Ingredient
     {
-        [JsonProperty("IngredientId")]
         public Guid IngredientId { get; set; }
-        [JsonProperty("Name")]
         public string Name { get; set; }
-        [JsonProperty("Price")]
         public double Price { get; set; }
-        [JsonProperty("Type")]
         public string Type { get; set; }
-        [JsonProperty("UnitType")]
         public string UnitType { get; set; }
-        [JsonProperty("Available")]
         public bool Available { get; set; }
-        [JsonProperty("Description")]
         public string Description { get; set; }
-
-
-
-
     }
 }
