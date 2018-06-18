fd19706c94d5309c48c31c8d71f363198aac8012
Khaled Sharaf Eldin <wi15b189@technikum-wien.at>
2018-06-18 23:18:50 +0200

    I as a Service user want to Query OrderContentPackagage including Customer, and Chocolates with ingrdients

---
+++ AppServiceHost/Configuration/CorsDomain.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppServiceHost.Configuration
+{
+    public class CorsDomain : ConfigurationElement
+    {
+        [ConfigurationProperty("Name", IsRequired = true)]
+        public string Name
+        {
+            //get { return Convert.ToString(this["Name"]); }
+            //set { this["Name"] = value; }
+
+            get { return "http://localhost:4200"; }
+            set { this["Name"] = value; }
+        }
+
+        [ConfigurationProperty("AllowMethods", IsRequired = true)]
+        public string AllowMethods
+        {
+            //get { return Convert.ToString(this["AllowMethods"]); }
+            //set { this["AllowMethods"] = value; }
+
+            get { return "POST"; }
+            set { this["AllowMethods"] = value; }
+        }
+
+        [ConfigurationProperty("AllowHeaders")]
+        public string AllowHeaders
+        {
+            //get { return Convert.ToString(this["AllowHeaders"]); }
+            //set { this["AllowHeaders"] = value; }
+
+            get { return "Content-Type"; }
+            set { this["AllowHeaders"] = value; }
+        }
+
+        [ConfigurationProperty("AllowCredentials")]
+        public bool AllowCredentials
+        {
+            //get { return Convert.ToBoolean(this["AllowCredentials"]); }
+            //set { this["AllowCredentials"] = value; }
+
+            get { return true; }
+            set { this["AllowCredentials"] = value; }
+        }
+    }
+}
---
+++ DataManagementWordPressDB/DataHandlerWP.cs
@@ -0,0 +1,39 @@
+﻿using DataManagementWordPressDB.DataModel;
+using SharedDataTypesWordpress;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataManagementWordPressDB
+{
+    public class DataHandlerWP
+    {
+        
+        WordPressEntities db = new WordPressEntities();
+
+
+        public List<WPOrder> QueryAllOrders()
+        {
+            return (from a in db.wp_posts
+                    join b in db.wp_postmeta on a.ID equals b.post_id
+                    where a.post_type.Contains("shop_order")
+                    select new WPOrder
+                    {
+                        ID = a.ID,
+                        post_status = a.post_status,
+                        post_title = a.post_title,
+                        post_date = a.post_date
+                        //meta_value = b.
+                    }).ToList();
+        }
+
+
+
+
+
+
+
+    }
+}
